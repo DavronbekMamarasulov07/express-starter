c64787d5e7984beed28d5e7e9d5be83cb74a059c
DavronbekMamarasulov07 <nilufar12345davron@icloud.com>
2026-02-02 13:23:10 +0500

    update server.js

--- src/server.js
+++ src/server.js
@@ -10,5 +10,7 @@ app.get("/", (req, res) => {
   res.json({ message: "API is working" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 4200;
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`),
+);
